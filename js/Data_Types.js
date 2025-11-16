6060531d7445e03859870978515a9c40e76e1647
Aakash21231382 <aakashkumar0728@gmail.com>
2025-11-16 21:27:57 +0530

    Data_Types java scripts

---
+++ js/Data_Types.js
@@ -0,0 +1,24 @@
+" use strict"; //treat all js code as newer version
+//alert (3+3) //we are uing nofejs, not browser
+//console.log(3+3);
+
+let name= "Aakash"
+let age = 28
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint 
+// String =>""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol  => unique
+
+
+// Object 
+console.log(typeof(name)); //typeof
+console.log(typeof(undefined)); //undefined
+console.log(typeof null); // object
+
+
