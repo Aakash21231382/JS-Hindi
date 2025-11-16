2922532910bcb2dc8c9639553b6fee0f0381fee5
Aakash21231382 <aakashkumar0728@gmail.com>
2025-11-16 21:27:32 +0530

    Variable to java scripts

---
+++ js/variable.js
@@ -0,0 +1,17 @@
+const accountId = 2343663
+let accountEmail = "aaskjfdha@gamil.com"
+var accountPassword = "475776"
+accountCity = "Jaipur"
+
+let accountState;
+
+accountEmail= "ajdg@gmail.com"
+accountPassword= "35trert"
+accountCity= "Bangular"
+console.log([accountEmail, accountPassword, accountCity,accountState]);
+/*
+ prefer not touse var 
+ because of issue in block scope and functional scope 
+
+
+*/
