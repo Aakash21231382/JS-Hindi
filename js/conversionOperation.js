0b0804bee66fdb1f840ee8fb9ada7e7f95c810ee
Aakash21231382 <aakashkumar0728@gmail.com>
2025-11-17 08:41:36 +0530

    conversionOperation js

---
+++ js/conversionOperation.js
@@ -0,0 +1,64 @@
+// let score = 33;
+// console.log(typeof (33));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+
+// "33" =>33
+
+//"33daf" = NaN
+
+// true=>1; false =>0
+// let isLoggedIn = 1
+// let boooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boooleanIsLoggedIn);
+//1=> true; 0=> false
+// ""=>false
+// "aakash" =>true
+
+
+// let str = 44
+// let stingNum= String(str)
+// console.log(stingNum)
+// console.log(typeof stingNum);
+
+
+
+//String
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let  str1= "hello"
+// let str2 = "Hitesh"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2" );
+// console.log("1" + 2 + 2);
+// console.log(2+ 3*4/5%2);
+
+// console.log(+true);
+// console.log(+"")
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 +2 
+
+let gameCounter = 100
+
+++gameCounter ;
+console.log(gameCounter);
+
+
+
+
+
