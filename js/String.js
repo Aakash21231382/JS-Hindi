8b342a99274985c5b69699640e1f4cd29ed9f48c
Aakash21231382 <aakashkumar0728@gmail.com>
2025-11-17 18:37:14 +0530

    String Java Scripts

---
+++ js/String.js
@@ -0,0 +1,30 @@
+const name = "aakash"
+const repoCount = 50;
+console.log(name +repoCount + "Value");
+console.log(`Hello  my name is ${name} and you is a ${repoCount} boy`)
+const gameName = new String ('aakash')
+console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase);
+// console.log(gameName.charAt([3]));
+// console.log(gameName.indexOf('k'));
+
+
+
+ const newString = gameName.substring(0,4)
+ console.log(newString);
+ const anotherString = gameName.slice(-4, 0)
+ console.log(anotherString)
+
+ const newStringOne = "    Aakash     "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ const url ="https://aakash.com/aakash%20kumar"
+ url.replace(url.replace)
+ console.log(url.includes('sunder'));
+ 
+ 
