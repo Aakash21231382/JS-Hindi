9ba32929e7d6e1b0393dd500703ac02853d59a56
Aakash21231382 <aakashkumar0728@gmail.com>
2025-12-14 18:14:23 +0530

    if.js

---
+++ js/if.js
@@ -0,0 +1,56 @@
+// if 
+//  const isUserloggedIn = true
+//  const temperature = 42
+//  if  (temperature === 42){
+//     console.log("less than 50");   
+//  } else {
+//      console.log("Temprature is grature than 50");
+//  }
+// console.log("execute");
+
+ 
+//  if ( 2 != 3){
+//     console.log("executed");
+    
+    
+//  }
+
+  //<, >, <=, >=, ==, !=, ===  ,  !==
+
+//   const score = 200
+//  if (score > 100 ) {
+//     let power = "fly "
+//     console.log(`User Power: ${power}`);
+    
+//  }
+
+//
+
+//const balance = 1000
+// if(balance >500) console.log("test");
+
+// if (balance < 500){
+//     console.log("less than 500");
+    
+// } else if(balance <750) {
+//     console.log("less than 750");
+    
+// } else if (balance <900) {
+//     console.log("less than 750");
+    
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+let loggedInFromGoogle = false
+let loggedInFromEmail = true
+if (userLoggedIn && debitCard && 6==6){
+    console.log("Allow to buy courses");    
+}
+if (loggedInFromEmail || loggedInFromGoogle){
+    console.log("User logged in ");
+    
+}
